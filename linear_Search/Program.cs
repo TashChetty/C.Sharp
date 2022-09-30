0837f1c18c7e6fe1d8a63ddab5ae94cfc94fdd1f
Natasha Chetty <natasha.chetty2@gmail.com>
2022-09-30 11:25:06 +0200

    linear_Search

---
+++ linear_Search/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class LinearSearch
+{
+
+
+    public static void Main()
+    {
+        int[] myData = { 30, 80, 25, 21 };
+        int searchData = 90;
+        int totalCount = myData.Count();
+        int i;
+        for (i = 0; i < totalCount; i++)
+        {
+
+            if (myData[i] == searchData)
+            {
+                Console.WriteLine("We found our data: " + searchData +  " in position: " + (i+1));
+                break;
+            }
+
+                
+        }
+            if (i == totalCount)
+                Console.WriteLine("We did not find our data: ");
+    }
+}
