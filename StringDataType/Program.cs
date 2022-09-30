3ecb4894d4b25ebaa9ce980755e9a2d8443cd3d9
Natasha Chetty <natasha.chetty2@gmail.com>
2022-09-30 11:15:08 +0200

    first commit

---
+++ StringDataType/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DataTypesString
+{
+    class datatypestring
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello World";
+            var bytecount = str.Length * sizeof(Char);
+            Console.WriteLine($"str Value: {str}");
+            Console.WriteLine($"str Size: {bytecount}");
+            Console.ReadKey();
+        }
+    }
+}
