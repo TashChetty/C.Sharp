24c120227584766194eb4fb61d2c42cfec2eb733
Natasha Chetty <natasha.chetty2@gmail.com>
2022-09-30 11:26:39 +0200

    charDataType

---
+++ CharDataType/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DataTypesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = 'B';
+            Console.WriteLine($"Char: " + (char)ch);
+            Console.WriteLine($"Equivalent Number: {(byte)ch}");
+            Console.WriteLine($"Char Minimum: {(int)char.MinValue} and Maximum: {(int)char.MaxValue}");
+            Console.WriteLine($"Char Size: {sizeof(char)} Byte");
+            Console.ReadKey();
+        }
+    }
+}
