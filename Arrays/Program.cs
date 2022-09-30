586caadf503b29c3cebd2ededba4697620390673
Natasha Chetty <natasha.chetty2@gmail.com>
2022-09-30 11:28:38 +0200

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class FindMax { 
+
+
+    public static void Main()
+{
+    int[] marks = { 25, 60, 95, 89, 62, 15 };
+    int largeNum = marks[0];
+
+    for (int i = 1; i < 6; i++)
+    {
+        if (marks[i] > largeNum)
+        {
+            largeNum = marks[i];
+
+        }
+    }
+    System.Console.WriteLine("The largest number is :  " + largeNum);
+
+}
+
+}
+
